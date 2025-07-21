9a550b57f90988bc566dab15786f015f828cf704
Sallah10 <107303495+Sallah10@users.noreply.github.com>
2025-07-21 09:03:15 +0900

    feat: other half done , perfection remaining and implementation

--- apps/web/app/(dashboard)/event-planner/events/page.tsx
+++ apps/web/app/(dashboard)/event-planner/events/page.tsx
@@ -1,9 +1,15 @@
 import React from 'react'
-import Image from 'next/image'
+import Image, { StaticImageData } from 'next/image'
 import eventImage from '../../../../public/event-planner/background.png'
 import back from '../../../../public/event-planner/arrow.png'
-import edit from '../../../../public/event-planner/edit.png'
+import editImage from '../../../../public/event-planner/edit.png'
 import image from '../../../../public/event-planner/image1.png'
+import placeholder from '../../../../public/event-planner/placeholder.png'
+import location from '../../../../public/event-planner/location.png'
+import time from '../../../../public/event-planner/time.png'
+import date from '../../../../public/event-planner/date.png'
+import sitting from '../../../../public/event-planner/sitting.png'
+import map from '../../../../public/event-planner/Full Map.png'
 
 // type Props = {}
 // { }: Props
@@ -12,6 +18,18 @@ function page() {
     stat: string,
     value: number
   }
+  type editType = {
+    imageUrl: StaticImageData,
+    tag: string
+  }
+
+  const edits: editType[] = [
+    { imageUrl: placeholder, tag: 'Edit' },
+    { imageUrl: placeholder, tag: 'Theme' },
+    { imageUrl: placeholder, tag: 'Messages' },
+    { imageUrl: placeholder, tag: 'Guests' },
+
+  ]
   const status: statusType[] = [{ stat: 'going', value: 0 }, { stat: 'pending ', value: 0 }, { stat: 'Not going', value: 0 }]
   return (
     <div className='px-6'>
@@ -27,7 +45,7 @@ function page() {
           <li>Budget</li>
         </ul>
       </header>
-      <main>
+      <main className='flex flex-col lg:flex-row gap-4 '>
         <section>
           <Image src={eventImage} alt='Event-Image' />
           <div className='flex flex-col md:flex-row my-4 gap-4 md:justify-between pb-4 border-b-2 border-gray-300'>
@@ -47,7 +65,7 @@ function page() {
             </div>
             <div className='flex gap-3 items-center'>
               <h2 className='text-[#2B2BCF] font-semibold text-lg '> Creator</h2>
-              <Image src={edit} alt='edit' height={20} width={20} />
+              <Image src={editImage} alt='edit' height={20} width={20} />
             </div>
           </div>
           <div>
@@ -68,8 +86,52 @@ function page() {
             </div>
           </div>
         </section>
-        <section>
-
+        <section className='mt-4 md:mt-0 '>
+          <div>
+            <h1 className='text-2xl font-semibold mb-2'>Oliver & Emily&apos;s Wedding</h1>
+            <div className='flex flex-col md:flex-row gap-4 my-4'>
+              <div className='flex items-center gap-2'>
+                <Image src={location} alt='location' />
+                <h2 className='flex flex-row gap-4 '>The Grand Hall. Rosewood Estate</h2>
+              </div>
+              <div className='flex items-center gap-2'>
+                <Image src={date} alt='date' />
+                <h2>Sat, Aug 20</h2>
+              </div>
+              <div className='flex items-center gap-2'>
+                <Image src={time} alt='time' />
+                <h2>
+                  3:00 PM - 10:00PM
+                </h2>
+              </div>
+            </div>
+            <div className='grid grid-cols-4 items-center'>
+              {
+                edits.map((edit, index) => (
+                  <div key={index} className='flex flex-col items-center text-gray-400'>
+                    <Image src={edit.imageUrl} alt={edit.tag} />
+                    <h2>{edit.tag}</h2>
+                  </div>
+                ))
+              }
+            </div>
+          </div>
+          <div className='flex flex-col p-4 bg-purple-700 items-center my-4 rounded-lg text-white-base text-md'>
+            <div className='flex gap-2 items-center justify-center'>
+              <Image src={sitting} alt='sitting' width={15} height={15} /> <h2>Create sitting chart</h2>
+            </div>
+            <p>Assign seats to guests and notify them</p>
+          </div>
+          <div>
+            <h1 className='text-black text-2xl  pb-4 border-b-2 border-gray-300 font-semibold my-4'>About</h1>
+            <p className='text-justify mb-4'>Celebrate the union of Oliver and Emily at the beautiful Rosewood Estate. Enjoy a romantic ceremony, followed by a gourmet dinner and lively dance. Join us for an evening of love, joy, and unforgettable memories as the couple begins their journey together.</p>
+          </div>
+          <div>
+            <h1 className='text-black text-2xl  pb-4 border-b-2 border-gray-300 font-semibold my-4'>Location</h1>
+            <h2 className='text-black font-semibold'>The Grand Hall, Rosewood Estate </h2>
+            <p className='text=gray-400 mb-4 '>123 Broadway Avenue, NY 10001</p>
+            <Image src={map} alt='map' />
+          </div>
         </section>
       </main>
     </div>
